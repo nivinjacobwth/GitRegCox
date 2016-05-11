c8ff3a81ec98af71c075d3dba7300c74840c0293
nivinjacobwth <nivinjacob@gmail.com>
2016-05-10 22:05:42 -0400

    Filename path change

--- Test/test.R
+++ Test/test.R
@@ -1,8 +1,8 @@
 install_github("nivinjacobwth/RegCox")
 library(RegCox)
 
-train_data <- read.csv("D:/MS/Wayne/Academics/Thesis/My R Package/Healthcare/Data/synthetic-1/synthetic1-survival_norm.csv")
-#train_data <- read.csv("D:/MS/Wayne/Academics/Thesis/My R Package/Healthcare/Data/breast_norm.csv")
+train_data <- read.csv("synthetic1-survival_norm.csv")
+#train_data <- read.csv("breast_norm.csv")
 
 
 train_time <- train_data$time
@@ -19,7 +19,7 @@ fit <- regcox(x, y, t, "Oscar", nfolds=5, lambda1=0.1, lambda2=0.1)
 fit <- regcox(x, y, t, "Fear", nfolds=5, tol=1e-3, lambda=0.1, itermax=100)
 fit <- regcox(x, y, t, "Cocktail", nfolds=5, tol=1e-3, lambda=0.1, itermax=100,alpha=.5, weight=1)
 
-filename = "D:/MS/Wayne/Academics/Thesis/My R Package/Healthcare/Data/breast_norm.csv"
+filename = "breast_norm.csv"
 auc <- arc(filename, regtype="Lasso", metric="AUC", nfolds=5, alpha=.5, lambda=.01 )
 print(auc)
 auc <- arc(filename, regtype="Enet", metric="AUC", nfolds=5, alpha=.5, lambda=.01 )
@@ -38,7 +38,7 @@ print(mse)
 mse <- arc(filename, regtype="Kernelnet", metric="MSE", nfolds=5, alpha=.5, lambda=.01 )
 print(mse)
 
-filename = "D:/MS/Wayne/Academics/Thesis/My R Package/Healthcare/Data/synthetic-1/synthetic1-survival_norm.csv"
+filename = "synthetic1-survival_norm.csv"
 auc <- arc(filename, regtype="Lasso", metric="AUC", nfolds=5, alpha=.5, lambda=.01 )
 print(auc)
 auc <- arc(filename, regtype="Enet", metric="AUC", nfolds=5, alpha=.5, lambda=.01 )
